98e7952c4fae9adca6541e7c7c33726aab5d62dd
Ricardo Metzger <r7metzger@gmail.com>
2024-11-22 13:35:04 -0500

    feat: cambiar a statefulwidget

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -10,18 +17,18 @@ class CounterScreen extends StatelessWidget {
         title: const Text('Counter Screen'),
         centerTitle: true,
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              '10',
-              style: TextStyle(
+              '$counter',
+              style: const TextStyle(
                 fontSize: 160,
                 fontWeight: FontWeight.w100,
               ),
             ),
-            Text(
+            const Text(
               'Clicks',
               style: TextStyle(
                 fontSize: 25,
@@ -31,7 +38,11 @@ class CounterScreen extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
         child: const Icon(Icons.plus_one_rounded),
       ),
     );
