f13b7207792eac743a45897e674798c2a20999f3
Ricardo Metzger <r7metzger@gmail.com>
2024-11-23 15:18:31 -0500

    feat: finalizacion de app contador

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_functions_screen.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: Colors.blue,
       ),
-      home: const CounterScreen(),
+      home: const CounterFunctionScreen(),
     );
   }
 }
